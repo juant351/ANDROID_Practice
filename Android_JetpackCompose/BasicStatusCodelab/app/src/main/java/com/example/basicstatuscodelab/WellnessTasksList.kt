923d5cfb852d9fcf4099414a2c92a7868c75fd34
juant351 <juan.torres.viloria@gmail.com>
2023-04-13 21:26:44 +0200

    Finished app BasicStatusCodelab

--- Android_JetpackCompose/BasicStatusCodelab/app/src/main/java/com/example/basicstatuscodelab/WellnessTasksList.kt
+++ Android_JetpackCompose/BasicStatusCodelab/app/src/main/java/com/example/basicstatuscodelab/WellnessTasksList.kt
@@ -3,7 +3,6 @@ package com.example.basicstatuscodelab
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
