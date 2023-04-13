55d82a2ef0fb2f284ae83db68267a64c38a049c2
juant351 <juant3501@gmail.com>
2023-04-13 11:30:01 +0200

    added last compose codelabs

---
+++ Android_JetpackCompose/BasicStatusCodelab/app/src/main/java/com/example/basicstatuscodelab/WellnessTask.kt
@@ -0,0 +1,3 @@
+package com.example.basicstatuscodelab
+
+data class WellnessTask(val id: Int, val label: String, var checked: Boolean = false)
---
+++ Android_JetpackCompose/BasicStatusCodelab/app/src/main/java/com/example/basicstatuscodelab/WellnessViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.basicstatuscodelab
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel: ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask> = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) =
+        tasks.find { it.id == item.id }?.let { task ->
+            task.checked = checked
+        }
+}
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
